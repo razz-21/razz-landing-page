d94ad46ac35f53c7e8fe2f41baefca0acb563a13
Ernesto Razo <iamryutadashi@gmail.com>
2021-04-02 12:45:29 +0800

    chore: set shorthand path for scss & separate navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./main-page/main-page.component";
+import { NavbarComponent } from './main-page/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
