b663792a1702bcaa246d4b21e2b89cd2b9c891b4
Ernesto Razo <iamryutadashi@gmail.com>
2021-04-02 15:09:57 +0800

    feat: about me ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./main-page/main-page.component";
-import { NavbarComponent } from './main-page/components/navbar/navbar.component';
-import { HeaderContentComponent } from './main-page/components/header-content/header-content.component';
+import { NavbarComponent } from "./main-page/components/navbar/navbar.component";
+import { HeaderContentComponent } from "./main-page/components/header-content/header-content.component";
+import { AboutMeComponent } from "./main-page/components/about-me/about-me.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     NavbarComponent,
-    HeaderContentComponent
+    HeaderContentComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
