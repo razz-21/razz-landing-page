d55f41e090fcab6f3298edeaf362227ba9f10473
Ernesto Razo <iamryutadashi@gmail.com>
2021-04-03 21:30:57 +0800

    feat: navbar for mobile screen

--- src/app/main-page/components/navbar/navbar.component.ts
+++ src/app/main-page/components/navbar/navbar.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild("navSideBar") navSideBar!: ElementRef;
+
+  navSideBarEl!: HTMLElement;
+
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.navSideBarEl = this.navSideBar.nativeElement;
+  }
+
+  toggleSidebar(): void {
+    const isActive = this.navSideBarEl.classList.contains("active");
+
+    !isActive
+      ? this.renderer.addClass(this.navSideBarEl, "active")
+      : this.renderer.removeClass(this.navSideBarEl, "active");
   }
 
 }
